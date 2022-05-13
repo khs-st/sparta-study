7842cb394c330a1d244e0d8300f0378b3561c009
KoHanSol <abc35572003@naver.com>
2022-05-13 22:13:45 +0900

    메모 목록의 시간을 조회 시간으로부터 24시간 이내로 수정 - LocalDateTime 이용

--- week03/src/main/java/com/sparta/week03/controller/MemoController.java
+++ week03/src/main/java/com/sparta/week03/controller/MemoController.java
@@ -7,6 +7,7 @@ import com.sparta.week03.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -23,7 +24,11 @@ public class MemoController {
 
     @GetMapping("/api/memos")
     public List<Memo> readMemo(){
-        return memoRepository.findAllByOrderByModifiedAtDesc();
+        //return memoRepository.findAllByOrderByModifiedAtDesc();
+        //타임라인 서비스가 불러오는 메모 목록의 시간을, 조회 시간으로부터 24시간 이내로
+        LocalDateTime start = LocalDateTime.now().minusDays(1); //하루전
+        LocalDateTime end = LocalDateTime.now(); //오늘
+        return memoRepository.findAllByModifiedAtBetweenOrderByModifiedAtDesc(start,end);
     }
 
     @DeleteMapping("/api/memos/{id}")
--- week03/src/main/java/com/sparta/week03/domain/MemoRepository.java
+++ week03/src/main/java/com/sparta/week03/domain/MemoRepository.java
@@ -2,8 +2,11 @@ package com.sparta.week03.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo,Long> {
     List<Memo> findAllByOrderByModifiedAtDesc();
+    //타임라인 서비스가 불러오는 메모 목록의 시간을, 조회 시간으로부터 24시간 이내로
+    List<Memo> findAllByModifiedAtBetweenOrderByModifiedAtDesc(LocalDateTime start, LocalDateTime end);
 }
