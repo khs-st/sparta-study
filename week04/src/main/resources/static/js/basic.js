30f8d11e1de53008a80ca6b3a75ed0bb2cd3be61
KoHanSol <abc35572003@naver.com>
2022-05-20 17:46:13 +0900

    [4주차] 상품 검색 기능 추가

--- week04/src/main/resources/static/js/basic.js
+++ week04/src/main/resources/static/js/basic.js
@@ -47,9 +47,27 @@ function execSearch() {
      * 검색결과 목록: #search-result-box
      * 검색결과 HTML 만드는 함수: addHTML
      */
+    $('#search-result-box').empty();
     // 1. 검색창의 입력값을 가져온다.
+    let query = $('#query').val();
     // 2. 검색창 입력값을 검사하고, 입력하지 않았을 경우 focus.
+    if (query == '') {
+        alert('검색어를 입력해주세요');
+        $('#query').focus();
+        return;
+    }
     // 3. GET /api/search?query=${query} 요청
+    $.ajax({
+        type : 'GET',
+        url : `/api/search?query=${query}`,
+        success : function (response){
+            for(let i=0; i<response.length; i++){
+                let itemDto = response[i];
+                let tempHtml = addHTML(itemDto);
+                $('#search-result-box').append(tempHtml);
+            }
+        }
+    })
     // 4. for 문마다 itemDto를 꺼내서 HTML 만들고 검색결과 목록에 붙이기!
 
 }
@@ -60,7 +78,23 @@ function addHTML(itemDto) {
      * image, title, lprice, addProduct 활용하기
      * 참고) onclick='addProduct(${JSON.stringify(itemDto)})'
      */
-    return ``
+    return ` <div id="search-result-box">
+        <div class="search-itemDto">
+            <div class="search-itemDto-left">
+                <img src="${itemDto.image}"alt="${itemDto.title}">
+            </div>
+            <div class="search-itemDto-center">
+                <div>${itemDto.title}</div>
+                <div class="price">
+                    ${numberWithCommas(itemDto.lprice)}
+                    <span class="unit">원</span>
+                </div>
+            </div>
+            <div class="search-itemDto-right">
+                <img src="/images/icon-save.png" alt="저장하기이미지" onClick='addProduct(${itemDto})'>
+            </div>
+        </div>
+    </div>`
 }
 
 function addProduct(itemDto) {
